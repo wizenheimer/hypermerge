54ee35b8c31f9e0bed5a05e9dae29498ecc60313
wizenheimer <xNayanKumar@gmail.com>
2025-03-30 20:31:33 +0530

    build: starter build

---
+++ app/page.tsx
@@ -0,0 +1,27 @@
+'use client';
+
+import Image from 'next/image';
+
+export default function Page() {
+    return (
+        <div
+            className="w-full min-h-screen flex items-center justify-center bg-white dark:bg-black transition-colors duration-200 flex-col p-4 gap-[32px]"
+            data-oid="aira6ev"
+        >
+            <div className="text-center text-gray-900 dark:text-gray-100 p-4" data-oid="4g9ksoy">
+                <h1
+                    className="text-4xl md:text-5xl font-semibold mb-4 tracking-tight"
+                    data-oid="1:sjll8"
+                >
+                    Welcome to your new Onlook website
+                </h1>
+                <p
+                    className="text-xl md:text-2xl text-gray-800 dark:text-gray-300"
+                    data-oid="-2n:8h_"
+                >
+                    This is a blank template – Feel free to edit it!
+                </p>
+            </div>
+        </div>
+    );
+}
